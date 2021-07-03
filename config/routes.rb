4f4cae7636f8ce67aa38c930c843a1ce7c6a1b83
pbsmith82 <pbsmith82@gmail.com>
2021-07-03 12:21:43 -0400

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :components
+  resources :ideas
+  resources :comments
+
+  resources :components do
+    resources :ideas, only: [:index, :new, :create]
+  end
+
+  resources :ideas do
+    resources :comments, only: [:index, :new, :create]
+  end
+
 end
